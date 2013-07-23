150eb1cf14b9d6fe2361e03492ee26dcb1a0788a
Hansen Qian <hansenq@fb.com>
2013-07-23 15:32:03 -0700

    Created a new workspace with two new projects, one for frontend, one for backend.

---
+++ PSFrontend/PSFrontend/PSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PSAppDelegate.h
+//  PSFrontend
+//
+//  Created by Hansen Qian on 7/23/13.
+//  Copyright (c) 2013 Pusheen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PSFrontend/PSFrontendTests/PSFrontendTests.m
@@ -0,0 +1,36 @@
+//
+//  PSFrontendTests.m
+//  PSFrontendTests
+//
+//  Created by Hansen Qian on 7/23/13.
+//  Copyright (c) 2013 Pusheen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PSFrontendTests : XCTestCase
+
+@end
+
+@implementation PSFrontendTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
